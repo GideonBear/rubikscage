4858a9882f41c0ff58fbc5bcdb8d0555bbc8841f
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-11-26 10:52:38 +0100

    c

--- src/backend/run.rs
+++ src/backend/run.rs
@@ -1,23 +1,10 @@
 use crate::cage::Cage;
 use crate::player::Player;
-use clap::Parser;
-use clap_num::number_range;
 
-fn players_parser(s: &str) -> Result<u8, String> {
-    number_range(s, 1, 4)
-}
-
-#[derive(Parser, Debug)]
-struct Args {
-    #[arg(short, long, value_parser=players_parser)]
-    players: u8,
-}
-
-pub fn run<P: Player>() {
-    let args = Args::parse();
+pub fn run<P: Player>(num_players: u8) {
     let mut cage = Cage::new();
 
-    let stocks: Vec<Vec<u8>> = match args.players {
+    let stocks: Vec<Vec<u8>> = match num_players {
         2 => vec![
             vec![0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2],
             vec![3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5],
@@ -42,7 +29,7 @@ pub fn run<P: Player>() {
         for player in &mut players {
             player.make_move(&mut cage);
             if let Some(won_color) = cage.check_win() {
-                let won_player = match args.players {
+                let won_player = match num_players {
                     2 => match won_color {
                         0..=2 => 0,
                         3..=5 => 1,
--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,8 +1,22 @@
 use crate::input_player::InputPlayer;
 use rubikscage::run::run;
+use clap::Parser;
+use clap_num::number_range;
 
 mod input_player;
 
+fn players_parser(s: &str) -> Result<u8, String> {
+    number_range(s, 1, 4)
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, value_parser=players_parser)]
+    players: u8,
+}
+
 fn main() {
-    run::<InputPlayer>()
+    let args = Args::parse();
+
+    run::<InputPlayer>(args.players);
 }
